97cf9805fccd5af80a44bb13205eeac42412d6d9
woraliss <72595543+woraliss@users.noreply.github.com>
2020-10-09 14:45:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ def index():
     a=os.environ['Authorization']
     try:
         f = open("student.csv", "r")
-       for line in f.readlines():
-            print(line)
-            a = line.split(",")
-           if(a[0]=="00001"):
-               return a[4]
-      f.close()
+            for line in f.readlines():
+                print(line)
+                a = line.split(",")
+            if(a[0]=="00001"):
+                return a[4]
+        f.close()
     except Exception:
         return "Could not read to file"
     
